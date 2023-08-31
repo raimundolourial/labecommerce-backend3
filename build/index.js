982576934dce6aa217415b8d50087afaab5c17dc
Amanda <amandaoliveirapolari@gmail.com>
2023-08-31 15:51:00 -0300

    [feat] exercise of typescript one completed

---
+++ build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const database_1 = require("./database");
+console.log('O aplicativo foi iniciado com sucesso!');
+console.table(database_1.users);
+console.table(database_1.products);
+//# sourceMappingURL=index.js.map
