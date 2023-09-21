d658e9a247dbeac923dd4eddb77958b7c213dabe
Amanda <amandaoliveirapolari@gmail.com>
2023-09-21 16:52:10 -0300

    [feat] exercise of data flow in the backend completed

--- README.md
+++ README.md
@@ -22,5 +22,166 @@
 ## Índice
 
 -   [1. Resumo do Projeto](#1-resumo-do-projeto)
+-   [2. Explicação da API](#2-explicação-da-api)
+    -   [2.1 URL Base](#21-url-base)
+    -   [2.2 Endpoints](#22-endpoints)
+        -   [2.2.1 Recuperar Todos os Usuários](#221-recuperar-todos-os-usuários)
+        -   [2.2.2 Recuperar Todos os Produtos](#222-recuperar-todos-os-produtos)
+        -   [2.2.3 Criar um Novo Usuário](#223-criar-um-novo-usuário)
+        -   [2.2.4 Criar um Novo Produto](#224-criar-um-novo-produto)
+        -   [2.2.5 Excluir um Usuário por ID](#225-excluir-um-usuário-por-id)
+        -   [2.2.6 Excluir um Produto por ID](#226-excluir-um-produto-por-id)
+        -   [2.2.7 Editar um Produto por ID](#227-editar-um-produto-por-id)
+    -   [2.3 Tratamento de Erros](#2-3-tratamento-de-erros)
 
 ## 1. Resumo do Projeto
+
+## 2. Explicação da API
+
+A API que foi desenvolvida para gerenciar usuários e produtos em um sistema de comércio eletrônico. Ela fornece uma variedade de endpoints que permitem realizar operações como recuperar informações de usuários e produtos, criar novos registros, excluir registros existentes e editar produtos(CRUD).
+
+A API foi desenvolvida utilizando tecnologias como Node.js, TypeScript e Express para criar um servidor robusto e escalável. Além disso, o Git e o GitHub foram utilizados para o controle de versionamento do código fonte, garantindo um desenvolvimento colaborativo e organizado. Para testar os endpoints, a ferramenta Postman pode ser empregada para verificar as respostas da API.
+
+Este projeto está atualmente em construção, e novos recursos e funcionalidades podem ser adicionados no futuro para melhorar sua capacidade e utilidade.
+
+### 2.1 URL Base
+
+A URL base para esta API é `http://localhost:3003`.
+
+### 2.2 Endpoints
+
+[Clique aqui para visualizar a documentação da API](https://documenter.getpostman.com/view/28316385/2s9YCBsofL#038765ca-c528-4237-a382-6730cdccb12c)
+
+A API fornece os seguintes endpoints para interagir com usuários e produtos:
+
+#### 2.2.1 Recuperar Todos os Usuários
+
+-   **Método HTTP:** GET
+-   **Descrição:** Recupera uma lista de todos os usuários.
+-   **Endpoint:** `http://localhost:3003/users`
+
+#### 2.2.2 Recuperar Todos os Produtos
+
+-   **Método HTTP:** GET
+-   **Descrição:** Recupera uma lista de todos os produtos.
+-   **Endpoint:** `http://localhost:3003/products`
+
+#### 2.2.3 Criar um Novo Usuário
+
+-   **Método HTTP:** POST
+-   **Descrição:** Cria um novo usuário com os dados fornecidos no corpo da solicitação.
+-   **Endpoint:** `http://localhost:3003/users`
+-   **Parâmetros solicitados:**<br>
+    `id`
+
+    -   Tipo: string
+    -   Deve ser único
+    -   Deve iniciar com "u"
+    -   Mínimo de 4 caracteres
+
+    `name`
+
+    -   Tipo: string
+    -   Mínimo de 2 caracteres
+
+    `email`
+
+    -   Tipo: string
+    -   Deve ser único
+    -   Deve usar um dos seguintes provedores: Gmail, Hotmail (Outlook)
+
+    `password`
+
+    -   Tipo: string
+    -   Mínimo de 6 dígitos
+
+#### 2.2.4 Criar um Novo Produto
+
+-   **Método HTTP:** POST
+-   **Descrição:** Cria um novo produto com os dados fornecidos no corpo da solicitação.
+-   **Endpoint:** `http://localhost:3003/products`
+-   **Parâmetros solicitados:**<br>
+    `id`
+
+    -   Tipo: string
+    -   Deve ser único
+    -   Deve iniciar com "prod"
+    -   Mínimo de 7 caracteres
+
+    `name`
+
+    -   Tipo: string
+    -   Mínimo de 2 caracteres
+
+    `price`
+
+    -   Tipo: number
+
+    `description`
+
+    -   Tipo: string
+
+    -   Mínimo de 2 caracteres
+
+    `imageUrl`
+
+    -   Tipo: string
+
+    -   Mínimo de 2 caracteres
+
+#### 2.2.5 Excluir um Usuário por ID
+
+-   **Método HTTP:** DELETE
+-   **Descrição:** Exclui um usuário com base no ID fornecido como parâmetro na URL.
+-   **Endpoint:** `http://localhost:3003/users/idUser`
+-   **Parâmetros solicitados:**<br>
+    `id`
+
+#### 2.2.6 Excluir um Produto por ID
+
+-   **Método HTTP:** DELETE
+-   **Descrição:** Exclui um produto com base no ID fornecido como parâmetro na URL.
+-   **Endpoint:** `http://localhost:3003/products/idProduct`
+-   **Parâmetros solicitados:**<br>
+    `id`
+
+#### 2.2.7 Editar um Produto por ID
+
+-   **Método HTTP:** PUT
+-   **Descrição:** Edita um produto com base no ID fornecido como parâmetro na URL e com os dados fornecidos no corpo da solicitação.
+-   **Endpoint:** `http://localhost:3003/products/idProduct`
+-   **Parâmetros solicitados:**<br>
+    `id`
+
+    -   Opcional
+    -   Tipo: string
+    -   Deve ser único
+    -   Deve iniciar com "prod"
+    -   Mínimo de 7 caracteres
+
+    `name`
+
+    -   Opcional
+    -   Tipo: string
+    -   Mínimo de 2 caracteres
+
+    `price`
+
+    -   Opcional
+    -   Tipo: number
+
+    `description`
+
+    -   Opcional
+    -   Tipo: string
+    -   Mínimo de 2 caracteres
+
+    `imageUrl`
+
+    -   Opcional
+    -   Tipo: string
+    -   Mínimo de 2 caracteres
+
+### 2. 3 Tratamento de Erros
+
+A API inclui tratamento de erros para lidar com várias situações, como validação de entrada, IDs duplicados e outros erros comuns. Isso garante que a API responda com mensagens de erro adequadas e status HTTP apropriados quando ocorrem problemas.
--- src/index.ts
+++ src/index.ts
@@ -1,160 +1,264 @@
-import {
-    createUser,
-    getAllUsers,
-    createProduct,
-    getAllProducts,
-    searchProductsByName,
-    products,
-    users,
-} from './database';
-
-// => Exercício da aula de express e APIs:
+import { products, users } from './database';
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { TProducts, TUsers } from './types';
+import {
+    CheckElementExists,
+    CheckEmail,
+    CheckPassword,
+    CheckPrefixId,
+    checkMinimumLength,
+    isNotEmpty,
+    isNumber,
+    isString,
+    isUniqueEmail,
+    isUniqueId,
+} from './validations';
 
-// => User:
-// createUser('u003', 'Astrodev', 'astrodev@email.com', 'astrodev99');
-
-// getAllUsers();
-
-// => Product:
-// createProduct(
-//     'prod003',
-//     'SSD gamer',
-//     349.99,
-//     'Acelere seu sistema com velocidades incríveis de leitura e gravação.',
-//     'https://images.unsplash.com/photo'
-// );
-
-// getAllProducts();
-
-// resultado (+) :
-// searchProductsByName('gamer');
-// resultado (-) :
-// searchProductsByName('aa');
-
-// => Exercício da aula de express e apis:
+// Configs Express:
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-
 app.listen(3003, () => {
     console.log('Servidor rodando na porta 3003');
 });
 
-// Teste
-app.get('/ping', (req: Request, res: Response) => {
-    res.send('Pong!');
-});
-
-// getAllUsers;
-app.get('/users', (req: Request, res: Response) => {
-    const result: TUsers[] = users;
-    res.status(200).send(result);
-});
-
-// getAllProducts;
-// app.get('/products', (req: Request, res: Response) => {
-//     const result: TProducts[] = products;
-//     res.status(200).send(result);
-// });
-
-// getAllProducts -> Refatorado
-app.get('/products', (req: Request, res: Response) => {
-    const query: string = req.query.q as string;
-    if (query !== undefined) {
-        const productsByName: TProducts[] = products.filter((product) =>
-            product.name.toLowerCase().includes(query.toLowerCase())
-        );
-        if (productsByName.length <= 0) {
-            res.status(404).send('Nenhum produto encontrado');
-        } else {
-            res.status(200).send(productsByName);
+// Funções:
+// ✔ getAllUsers:
+app.get('/users', (req: Request, res: Response): void => {
+    try {
+        const result: TUsers[] = users;
+        res.status(200).send(result);
+    } catch (error) {
+        console.log(error);
+        if (error instanceof Error) {
+            res.send(error.message);
         }
-    } else {
-        res.status(200).send(products);
     }
 });
 
-// createUser
-app.post('/users', (req: Request, res: Response) => {
-    const { id, name, email, password }: TUsers = req.body;
-    const newUser: TUsers = {
-        id,
-        name,
-        email,
-        password,
-        createdAt: new Date().toLocaleString(),
-    };
-    users.push(newUser);
-    res.status(201).send('Cadastro realizado com sucesso');
+// ✔ getAllProducts
+app.get('/products', (req: Request, res: Response): void => {
+    try {
+        const query: string = req.query.q as string;
+
+        if (query !== undefined) {
+            checkMinimumLength(query, 'Query Params', 1, res);
+            const productsByName: TProducts[] = products.filter((product) =>
+                product.name.toLowerCase().includes(query.toLowerCase())
+            );
+            if (productsByName.length <= 0) {
+                res.status(404).send('Nenhum produto encontrado');
+            } else {
+                res.status(200).send(productsByName);
+            }
+        } else {
+            res.status(200).send(products);
+        }
+    } catch (error) {
+        if (error instanceof Error) {
+            res.send(error.message);
+        }
+    }
 });
 
-// createProduct
+// ✔ createUser
+app.post('/users', (req: Request, res: Response): void => {
+    try {
+        const { id, name, email, password }: TUsers = req.body;
+
+        // => ID
+        isString(id, 'Id', res);
+        isNotEmpty(id, 'Id', res);
+        isUniqueId(id, 'Id', users, res);
+        CheckPrefixId(id, 'Id', 'u', res);
+        checkMinimumLength(id, 'Id', 4, res);
+
+        // => NAME
+        isString(name, 'Name', res);
+        isNotEmpty(name, 'Name', res);
+        checkMinimumLength(name, 'Name', 2, res);
+
+        // => EMAIL
+        isString(email, 'Email', res);
+        isNotEmpty(email, 'Email', res);
+        isUniqueEmail(email, 'Email', users, res);
+        CheckEmail(email, 'Email', res);
+
+        // => PASSWORD
+        isString(password, 'Senha', res);
+        isNotEmpty(password, 'Senha', res);
+        // Por enquanto usando apenas pra senha ter ao menos 6 dígitos
+        CheckPassword(password, 'Senha', res);
+
+        const newUser: TUsers = {
+            id,
+            name,
+            email,
+            password,
+            createdAt: new Date().toLocaleString(),
+        };
+        users.push(newUser);
+        res.status(201).send('Cadastro realizado com sucesso');
+    } catch (error) {
+        if (error instanceof Error) {
+            res.send(error.message);
+        }
+    }
+});
+
+// ✔ createProduct
 app.post('/products', (req: Request, res: Response) => {
-    const { id, name, price, description, imageUrl }: TProducts = req.body;
-    const newProduct: TProducts = {
-        id,
-        name,
-        price,
-        description,
-        imageUrl,
-    };
-    products.push(newProduct);
-    res.status(201).send('Produto cadastrado com sucesso');
+    try {
+        const { id, name, price, description, imageUrl }: TProducts = req.body;
+        const newProduct: TProducts = {
+            id,
+            name,
+            price,
+            description,
+            imageUrl,
+        };
+
+        // => ID
+        isString(id, 'Id', res);
+        isNotEmpty(id, 'Id', res);
+        isUniqueId(id, 'Id', products, res);
+        CheckPrefixId(id, 'Id', 'prod', res);
+        checkMinimumLength(id, 'Id', 7, res);
+
+        // => NAME
+        isString(name, 'Name', res);
+        isNotEmpty(name, 'Name', res);
+        checkMinimumLength(name, 'Name', 2, res);
+
+        // => PRICE
+        isNumber(price, 'Preço', res);
+
+        // => DESCRIPTION
+        isString(description, 'Descrição', res);
+        isNotEmpty(description, 'Descrição', res);
+        checkMinimumLength(description, 'Descrição', 2, res);
+
+        // => IMAGEMURL
+        isString(imageUrl, 'URL da imagem', res);
+        isNotEmpty(imageUrl, 'URL da imagem', res);
+        checkMinimumLength(imageUrl, 'URL da imagem', 2, res);
+
+        products.push(newProduct);
+        res.status(201).send('Produto cadastrado com sucesso');
+    } catch (error) {
+        if (error instanceof Error) {
+            res.send(error.message);
+        }
+    }
 });
 
-// deleteUserById
+// ✔ deleteUserById
 app.delete('/users/:id', (req: Request, res: Response) => {
-    const id = req.params.id;
-    const indexToDelete = users.findIndex((user) => user.id === id);
-    if (indexToDelete >= 0) {
-        users.splice(indexToDelete, 1);
-    } else {
-        console.log(`O id ${id} não existe`);
+    try {
+        const id = req.params.id;
+        CheckElementExists(id, 'Id', users, res);
+        const indexToDelete = users.findIndex((user) => user.id === id);
+        if (indexToDelete >= 0) {
+            users.splice(indexToDelete, 1);
+        } else {
+            console.log(`O id ${id} não existe`);
+        }
+        res.status(200).send('Usuário apagado com sucesso');
+    } catch (error) {
+        if (error instanceof Error) {
+            res.send(error.message);
+        }
     }
-    res.status(200).send({
-        message: `User apagado com sucesso`,
-    });
 });
 
-// deleteProductById
+// ✔ deleteProductById
 app.delete('/products/:id', (req: Request, res: Response) => {
-    const id = req.params.id;
-    const indexToDelete = products.findIndex((product) => product.id === id);
-    if (indexToDelete >= 0) {
-        products.splice(indexToDelete, 1);
-    } else {
-        console.log(`O id ${id} não existe`);
+    try {
+        const id = req.params.id;
+        CheckElementExists(id, 'Id', products, res);
+        const indexToDelete = products.findIndex(
+            (product) => product.id === id
+        );
+        if (indexToDelete >= 0) {
+            products.splice(indexToDelete, 1);
+        } else {
+            console.log(`O id ${id} não existe`);
+        }
+        res.status(200).send('Produto apagado com sucesso');
+    } catch (error) {
+        if (error instanceof Error) {
+            res.send(error.message);
+        }
     }
-    res.status(200).send({
-        message: `Produto apagado com sucesso`,
-    });
 });
 
-// editProductById
+// ✔ editProductById
 app.put('/products/:id', (req: Request, res: Response) => {
-    const id = req.params.id;
-    const newId = req.body.id as string | undefined;
-    const newName = req.body.name as string | undefined;
-    const newPrice = req.body.price as number | undefined;
-    const newDescription = req.body.description as string | undefined;
-    const newImageUrl = req.body.imageUrl as string | undefined;
+    try {
+        const id = req.params.id;
+        const newId = req.body.id as string | undefined;
+        const newName = req.body.name as string | undefined;
+        const newPrice = req.body.price as number | undefined;
+        const newDescription = req.body.description as string | undefined;
+        const newImageUrl = req.body.imageUrl as string | undefined;
+        CheckElementExists(id, 'Id', products, res);
 
-    const product = products.find((product) => product.id === id);
+        const product = products.find((product) => product.id === id);
 
-    if (product) {
-        product.id = newId || product.id;
-        product.name = newName || product.name;
-        product.price =
-            newPrice !== undefined && !isNaN(Number(newPrice))
-                ? newPrice
-                : product.price;
-        product.description = newDescription || product.description;
-        product.imageUrl = newImageUrl || product.imageUrl;
+        if (product) {
+            if (newId !== undefined) {
+                isString(newId, 'Novo Id', res);
+                isNotEmpty(newId, 'Novo Id', res);
+                isUniqueId(newId, 'Novo Id', products, res);
+                CheckPrefixId(newId, 'Novo Id', 'prod', res);
+                checkMinimumLength(newId, 'Novo Id', 7, res);
+                product.id = newId;
+            } else {
+                product.id = product.id;
+            }
+
+            if (newName !== undefined) {
+                isString(newName, 'Novo Name', res);
+                isNotEmpty(newName, 'Novo Name', res);
+                checkMinimumLength(newName, 'Novo Name', 2, res);
+                product.name = newName;
+            } else {
+                product.name = product.name;
+            }
+
+            if (newPrice !== undefined) {
+                isNumber(newPrice, 'Novo Preço', res);
+                isNotEmpty(newPrice, 'Novo Preço', res);
+                product.price = newPrice;
+            } else {
+                product.price = product.price;
+            }
+
+            if (newDescription !== undefined) {
+                isString(newDescription, 'Nova Descrição', res);
+                isNotEmpty(newDescription, 'Nova Descrição', res);
+                checkMinimumLength(newDescription, 'Nova Descrição', 2, res);
+                product.description = newDescription;
+            } else {
+                product.description = product.description;
+            }
+
+            if (newImageUrl !== undefined) {
+                isString(newImageUrl, 'Nova URL da imagem', res);
+                isNotEmpty(newImageUrl, 'Nova URL da imagem', res);
+                checkMinimumLength(newImageUrl, 'Nova URL da imagem', 2, res);
+                product.imageUrl = newImageUrl;
+            } else {
+                product.imageUrl = product.imageUrl;
+            }
+        }
+
+        res.status(200).send('Atualização realizada com sucesso');
+    } catch (error) {
+        if (error instanceof Error) {
+            res.send(error.message);
+        }
     }
-
-    res.status(200).send({ message: 'Atualização realizada com sucesso' });
 });
